7981aa25e4d56e8bc551151e9f0afed88e4c12d7
Rutwik Balkhande <rutwikbalkhande42@gmail.com>
2025-11-28 17:07:27 +0530

    add bracket program

--- src/string/a.java
+++ src/string/a.java
@@ -1,11 +1,23 @@
 package string;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class a {
     public static void main(String[] args) {
 
+        String str = "Java Hungryy";
 
+        Map<Character, Integer> map= new HashMap<>();
+
+        for(char ch: str.toCharArray()){
+            if(map.containsKey(ch))
+            {
+                map.put(ch, map.get(ch) +1 );
+            }
+            else {
+                map.put(ch ,+1);
+            }
+        }
+        System.out.println(map);
     }
-}
+    }
